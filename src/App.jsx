3a9c621c21548f4c927d2981fbdcc4807be7c5b4
moses-mativo <clishtivo@gmail.com>
2023-04-12 17:43:26 +0300

    Button Component
    Creating a reusable Button Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Button } from './components/Buttons/Button'
 
 function App() {
   return (
     <div className="App">
       <h1>My Todo List Start</h1>
+      <Button label="Add Todo" onClick={() => {
+        return alert("Configure Add Todo");
+      }} primary size="large" />
     </div>
   )
 }
---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import './button.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  const mode = primary ? 'todo-list-button--primary' : 'todo-list-button--secondary';
+  return (
+    <button
+      className={['todo-list-button', `todo-list-button--${size}`, mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      type="button"
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool.isRequired,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
